3898a25927717678497e0a7cf477a5565da87587
Reverier-Xu <reverier.xu@outlook.com>
2023-09-12 13:07:00 +0800

    :construction: wiki

--- web/src/routes/wiki/+layout.svelte
+++ web/src/routes/wiki/+layout.svelte
@@ -2,9 +2,13 @@
   import RxButton from '$lib/components/RxButton.svelte'
   import { fly } from 'svelte/transition'
   import { quintOut } from 'svelte/easing'
+  import WikiSidebar from '$lib/blocks/WikiSidebar.svelte'
+    import type { WikiEntry } from '$lib/models/wiki'
 
   let toggleSidebar = false
   let screenWidth: number
+  let wikiEntries: WikiEntry[] = []
+  let activeId: number
 
   $: showSidebar = screenWidth > 1024 // lg
 </script>
@@ -13,7 +17,10 @@
 
 <div class="flex-1 flex flex-row">
   {#if showSidebar}
-    <div class="w-1/4 min-w-[400px] max-w-[512px] bg-base-100/40 backdrop-blur border-r border-r-base-content/5" />
+    <div class="fixed w-1/4 h-[calc(100vh_-_4rem)] min-w-[400px] max-w-[512px] bg-base-100/40 backdrop-blur border-r border-r-base-content/5 shadow">
+      <WikiSidebar bind:wiki={wikiEntries} bind:activeId />
+    </div>
+    <div class="w-1/4"></div>
   {:else}
     <RxButton
       size="lg"
@@ -30,9 +37,11 @@
   </div>
   {#if toggleSidebar && !showSidebar}
     <div
-      class="fixed w-full max-w-[400px] h-full backdrop-blur-xl bg-base-100/40 border-r border-r-base-content/5"
+      class="fixed w-full max-w-[400px] h-[calc(100vh_-_4rem)] overflow-hidden backdrop-blur bg-base-100/40 border-r border-r-base-content/5 shadow"
       transition:fly={{ delay: 100, duration: 300, x: -256, y: 0, opacity: 0, easing: quintOut }}
-    />
+    >
+      <WikiSidebar bind:wiki={wikiEntries} bind:activeId />
+    </div>
     <RxButton
       size="lg"
       class="fixed right-6 bottom-6"
