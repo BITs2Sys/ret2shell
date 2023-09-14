c50cc9c51673c053c56a9deda9368b4b25ae9d62
dx3906 <21009201351@stu.xidian.edu.cn>
2023-09-14 17:15:27 +0800

    :sparkles: notification api

--- server/src/controller/game/notification.rs
+++ server/src/controller/game/notification.rs
@@ -1,6 +1,96 @@
-use crate::controller::GlobalState;
-use axum::Router;
+use crate::{
+    controller::{layer::auth, GlobalState},
+    entity::{notification, user::Permission},
+};
+use axum::{
+    extract::{Path, Query, State},
+    middleware,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use hyper::StatusCode;
+use sea_orm::{DatabaseConnection, DbErr};
+use serde::{Deserialize, Serialize};
+use tracing::error;
 
 pub fn router(_state: &GlobalState) -> Router<GlobalState> {
     Router::new()
+        .route("/", post(create_notification).delete(delete_notification))
+        .route_layer(middleware::from_fn(auth::permission_required_all!(
+            Permission::Organize
+        )))
+        .route("/", get(get_notification_list))
+}
+
+async fn create_notification(
+    State(ref conn): State<DatabaseConnection>,
+    Json(notification): Json<notification::Model>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    match notification::create_notification(conn, notification).await {
+        Ok(_) => Ok(StatusCode::CREATED),
+        Err(DbErr::RecordNotFound(_)) => Err((StatusCode::NOT_FOUND, "game not found")),
+        Err(e) => {
+            error!("Failed to create notification: {}", e);
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to create notification",
+            ))
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct NotificationListQuery {
+    pub page: Option<u64>,
+    pub per_page: Option<u64>,
+}
+
+#[derive(Serialize)]
+struct NotificationList {
+    pub notifications: Vec<notification::Model>,
+    pub total: u64,
+}
+
+async fn get_notification_list(
+    State(ref conn): State<DatabaseConnection>,
+    Query(params): Query<NotificationListQuery>,
+    Path(game_id): Path<i64>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    let page = params.page.unwrap_or(1);
+    let per_page = params.per_page.unwrap_or(10);
+    if page < 1 || per_page < 1 {
+        error!("Invalid page={} or per_page={}", page, per_page);
+        return Err((StatusCode::BAD_REQUEST, "invalid parameters"));
+    }
+    match notification::get_notification_page(conn, page, per_page, game_id).await {
+        Ok((notifications, total)) => Ok(Json(NotificationList {
+            notifications,
+            total,
+        })),
+        Err(e) => {
+            error!("Failed to get notification list: {}", e);
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to get notification list",
+            ))
+        }
+    }
+}
+
+async fn delete_notification(
+    State(ref conn): State<DatabaseConnection>,
+    Query(notification_id): Query<i64>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    match notification::delete_notification(conn, notification_id).await {
+        Ok(_) => Ok(StatusCode::NO_CONTENT),
+        Err(DbErr::RecordNotFound(_)) => Err((StatusCode::NOT_FOUND, "notification not found")),
+        Err(e) => {
+            error!("Failed to delete notification: {}", e);
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to delete notification",
+            ))
+        }
+    }
 }
