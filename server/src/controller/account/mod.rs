f3bf327dc9be04d55bb375ddfcc64a387314daec
dx3906 <21009201351@stu.xidian.edu.cn>
2023-10-09 22:27:59 +0800

    :bug: fix resend_verification_email api

--- server/src/controller/account/mod.rs
+++ server/src/controller/account/mod.rs
@@ -529,10 +529,10 @@ async fn resend_verification_email(
         }
     }
     let verification_id = nanoid!(21, &alphabet::SAFE);
-    match cache::Email::add_freq_limit(cache, &user.email.clone().unwrap_or_default()).await {
+    match cache::Email::add_validation(cache, &verification_id, &user.email.clone().unwrap_or_default()).await {
         Ok(_) => {}
         Err(err) => {
-            error!("Failed to add email limit: {}", err);
+            error!("Failed to add email validation: {}", err);
         }
     }
     let email = Email {
