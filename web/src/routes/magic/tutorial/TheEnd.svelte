f2c3da84acf8c59c46026c262f27d40bec696660
Reverier-Xu <reverier.xu@outlook.com>
2023-10-31 12:46:21 +0800

    :construction: update tutorial

--- web/src/routes/magic/tutorial/TheEnd.svelte
+++ web/src/routes/magic/tutorial/TheEnd.svelte
@@ -19,13 +19,18 @@
 </script>
 
 {#if !goNext}
-  <div class="flex-1 flex flex-col justify-center items-center space-y-24" out:fly={{ y: -32, duration: 500 }}>
+  <div class="flex-1 flex flex-col justify-center items-center space-y-8" out:fly={{ y: -32, duration: 500 }}>
     <h1 class="text-4xl flex flex-row items-center space-x-8 font-bold" in:fly={{ y: 32, duration: 2000 }}>
       <LogoAnimate width={80} height={80} />
       <span>THE END But...</span>
     </h1>
-    <p class="w-full max-w-2xl text-lg" in:fly={{ y: 32, duration: 1000, delay: 500 }}>{$i18n.t('tutorial.theEnd')}</p>
-    <div class="flex flex-col space-y-4" in:fly={{ y: 16, duration: 1000, delay: 2000 }}>
+    <p class="text-lg w-full max-w-3xl indent-8" in:fly={{ y: 32, duration: 1000, delay: 500 }}>
+      {$i18n.t('tutorial.doNotProfit')}
+    </p>
+    <p class="w-full max-w-3xl text-lg indent-8" in:fly={{ y: 32, duration: 1000, delay: 1000 }}>
+      {$i18n.t('tutorial.theEnd')}
+    </p>
+    <div class="flex flex-col space-y-4 !mt-24" in:fly={{ y: 16, duration: 1000, delay: 2000 }}>
       <RxButton size="lg" on:click={next}>
         <span class="icon-[fluent--thumb-like-20-regular] w-5 h-5"></span>
         <span>{$i18n.t('tutorial.end')}</span>
