ad7605e8093017feb0e8891ce48720252ce3e310
Reverier Xu <reverier.xu@outlook.com>
2024-12-15 17:02:07 +0800

    :art: add types for onExit

--- web/src/routes/layout.tsx
+++ web/src/routes/layout.tsx
@@ -529,7 +529,7 @@ export default function (props: { children?: JSX.Element }) {
       {props.children}
       <Toasts />
       <Transition
-        onExit={async (el, done) => {
+        onExit={async (el: HTMLElement, done: () => void) => {
           const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
             duration: 300,
           });
