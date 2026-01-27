5b339bc9fd3b7f8d7ced3f6e04f878de0aa87f77
Reverier-Xu <reverier.xu@woooo.tech>
2026-01-28 04:00:10 +0800

    :bug: fix git opeartions icon pre-compiling

--- web/src/lib/utils/gitmoji.ts
+++ web/src/lib/utils/gitmoji.ts
@@ -1,19 +1,19 @@
 const gitmojiMap: Record<string, string> = {
-  ":sparkles:": "sparkles",
-  ":building_construction:": "building-construction",
-  ":package:": "package",
-  ":fire:": "fire",
-  ":speech_balloon:": "speech-balloon",
-  ":memo:": "memo",
-  ":tada:": "party-popper",
-  ":arrow_up:": "up-arrow",
+  ":sparkles:": "icon-[fluent-emoji-flat--sparkles]",
+  ":building_construction:": "icon-[fluent-emoji-flat--building-construction]",
+  ":package:": "icon-[fluent-emoji-flat--package]",
+  ":fire:": "icon-[fluent-emoji-flat--fire]",
+  ":speech_balloon:": "icon-[fluent-emoji-flat--speech-balloon]",
+  ":memo:": "icon-[fluent-emoji-flat--memo]",
+  ":tada:": "icon-[fluent-emoji-flat--party-popper]",
+  ":arrow_up:": "icon-[fluent-emoji-flat--up-arrow]",
 };
 
 export function transformGitmoji(message: string): { icon?: string; text: string } {
   for (const [gitmoji, icon] of Object.entries(gitmojiMap)) {
     if (message.startsWith(gitmoji)) {
       return {
-        icon: `icon-[fluent-emoji-flat--${icon}]`,
+        icon,
         text: message.slice(gitmoji.length).trimStart(),
       };
     }
